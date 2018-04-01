1315be71aaefd5b69114efd0c384d4739e68ba5a
Prabhat Sharma <hi.prabhat@gmail.com>
2018-04-01 07:36:16 -0700

    fixed folder name for sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 sync ./public/ s3://biradaree.in --acl public-read --profile personal
+aws s3 sync . s3://biradaree.in --acl public-read --profile personal
 
 aws cloudfront create-invalidation --distribution-id=E2K398AVP9409Z --paths=/ --profile personal
